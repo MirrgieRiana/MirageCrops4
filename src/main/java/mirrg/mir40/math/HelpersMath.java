2fc2356afd9d48e164abcfe1163d64c2b303050a
MirrgieRiana <devnull@localhost>
2014-09-13 21:40:39 +0900

    lledo il mir40.math

---
+++ src/main/java/mirrg/mir40/math/HelpersMath.java
@@ -0,0 +1,32 @@
+package mirrg.mir40.math;
+
+import java.util.Random;
+
+public class HelpersMath
+{
+
+	public static int floorRandom(double value)
+	{
+		int integer = (int) Math.floor(value);
+		double addition = value - integer;
+
+		if (Math.random() < addition) {
+			integer++;
+		}
+
+		return integer;
+	}
+
+	public static int floorRandom(double value, Random random)
+	{
+		int integer = (int) Math.floor(value);
+		double addition = value - integer;
+
+		if (random.nextDouble() < addition) {
+			integer++;
+		}
+
+		return integer;
+	}
+
+}
