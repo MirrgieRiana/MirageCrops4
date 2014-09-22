66f0d67061410a332ac629a6bb7eb53ae3bd3590
MirrgieRiana <devnull@localhost>
2014-09-22 23:06:44 +0900

    Version4.0.1(Torch Update)
    ・弱い松明追加（5カルサイト+2松明）

--- src/main/java/mirrg/miragecrops4/core/ModMirageCrops4.java
+++ src/main/java/mirrg/miragecrops4/core/ModMirageCrops4.java
@@ -18,7 +18,7 @@ public class ModMirageCrops4 extends ModAbstract
 {
 
 	public static final String MODID = "miragecrops4";
-	public static final String VERSION = "4.0.0";
+	public static final String VERSION = "4.0.1";
 	public static final String NAME = "MirageCrops4";
 	public static final String DEPENDENCIES =
 		"after:IC2;" +
