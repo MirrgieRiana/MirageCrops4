2588f669d4c51fdf677e028f0fe32d5c3101bea0
MirrgieRiana <devnull@localhost>
2014-09-22 21:27:32 +0900

    formato

--- src/main/java/mirrg/miragecrops4/core/oregen/ModuleOregenBase.java
+++ src/main/java/mirrg/miragecrops4/core/oregen/ModuleOregenBase.java
@@ -50,33 +50,33 @@ public abstract class ModuleOregenBase extends ModuleAbstract
 	protected void createMetaBlock(IEnumGlobs[] globs, BlockMulti blockMulti)
 	{
 		for (int i = 0; i < globs.length; i++) {
-	
+
 			// グロブの設定
 			((GlobAbstract) globs[i].getGlob()).setName(((Enum) globs[i]).name());
-	
+
 			// メタブロックの作成
 			Metablock metablock = new Metablock();
-	
+
 			// マルチブロックにメタブロックを登録
 			blockMulti.multibase.bind(i, metablock);
-	
+
 			// グロブにアイテムスタックを登録
 			((GlobAbstract) globs[i].getGlob()).put(ItemsOregen.slotOre, new ItemStack(blockMulti, 1, i));
-	
+
 			// メタブロックの設定
 			{
 				String unlocalizedName = HelpersGlob.getDictionaryName(ItemsOregen.slotOre, globs[i].getGlob());
-	
+
 				metablock.unlocalizedName = unlocalizedName;
 				if (getMod().isClient()) {
 					metablock.iconName = getMod().getModId() + ":" + getModuleName() + "/" + unlocalizedName;
 				}
-	
+
 				// 鉱石辞書に登録
 				OreDictionary.registerOre(unlocalizedName, new ItemStack(blockMulti, 1, i));
-	
+
 			}
-	
+
 		}
 	}
 
@@ -84,16 +84,16 @@ public abstract class ModuleOregenBase extends ModuleAbstract
 		String biome)
 	{
 		WorldGeneratorXZOre iWorldGeneratorXY;
-	
+
 		iWorldGeneratorXY = new WorldGeneratorXZOre(
 			CountPer.CUBE, countPerCube, minHeight, maxHeight,
 			new WorldGeneratorXYZOre(numberOfBlocks, ore));
 		GameRegistry.registerWorldGenerator(iWorldGeneratorXY, 801);
-	
+
 		if (biome != null) {
 			iWorldGeneratorXY.setFilter(new FilterBiome(biome));
 		}
-	
+
 		GameRegistry.registerWorldGenerator(iWorldGeneratorXY, 801);
 	}
 
