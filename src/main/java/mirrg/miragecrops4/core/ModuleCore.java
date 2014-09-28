4218980dba1b5d0074e22429231889aa1050eec2
MirrgieRiana <devnull@localhost>
2014-09-29 03:52:05 +0900

    lledo il cropjau

--- src/main/java/mirrg/miragecrops4/core/ModuleCore.java
+++ src/main/java/mirrg/miragecrops4/core/ModuleCore.java
@@ -1,16 +1,25 @@
 package mirrg.miragecrops4.core;
 
 import static mirrg.miragecrops4.api.oregen.ItemsOregen.*;
+import ic2.api.crops.CropCard;
+import ic2.api.crops.Crops;
 import mirrg.mir34.modding.IMod;
 import mirrg.mir34.modding.ModuleAbstract;
 import mirrg.mir40.glob.api.HelpersGlob;
 import mirrg.miragecrops4.api.oregen.ItemsOregen;
 import mirrg.miragecrops4.api.oregen.ItemsOregen.EnumGlobsCalciteGroup;
+import mirrg.miragecrops4.core.crop.CropMirage;
+import mirrg.miragecrops4.core.crop.HandlerHarvestOneItem;
+import mirrg.miragecrops4.core.crop.HandlerSpritesBasic;
+import mirrg.miragecrops4.core.crop.HandlerSpritesWrapping;
+import mirrg.miragecrops4.core.fairy.crop.CropMirageFairyHousing;
+import mirrg.miragecrops4.core.fairy.crop.CropMirageFairyWorkplace;
 import net.minecraft.block.Block;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemArmor;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.oredict.ShapedOreRecipe;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
@@ -62,11 +71,69 @@ public class ModuleCore extends ModuleAbstract
 
 	}
 
+	public static CropCard cropSarracenia;
+	public static CropCard cropLightningSarracenia;
+	public static CropCard cropRoseQuartz;
+
+	protected <T extends CropCard> T registerCrop(int id, T crop)
+	{
+		Crops.instance.registerCrop(crop, id);
+		return crop;
+	}
+
+	protected void registerCrops()
+	{
+		cropSarracenia = registerCrop(80, new CropMirage());
+		cropLightningSarracenia = registerCrop(81, new CropMirageFairyHousing());
+		cropRoseQuartz = registerCrop(100, new CropMirageFairyWorkplace());
+	}
+
+	protected void configureCrops()
+	{
+		String maskMiragecrops4 = "miragecrops4:crop/blockCrop.%name%.%size%";
+
+		CropMirage crop;
+
+		crop = (CropMirage) cropSarracenia;
+		crop.setStatusRegular("Sarracenia", 3, "Mirrgie Riana");
+		crop.setStatusCrossing(0, 0, 3, 1, 2, "Sarracenia");
+		crop.setMaxSize(5);
+		crop.setHandlerSprites(
+			new HandlerSpritesBasic(crop, maskMiragecrops4));
+		crop.setHandlerHarvest(
+			new HandlerHarvestOneItem(crop, new ItemStack(craftingLeaf)));
+
+		crop = (CropMirage) cropLightningSarracenia;
+		crop.setStatusRegular("Lightning Sarracenia", 5, "Mirrgie Riana");
+		crop.setStatusCrossing(1, 0, 3, 2, 2, "Sarracenia", "Lightning", "Purple");
+		crop.setMaxSize(5);
+		crop.setHandlerSprites(
+			new HandlerSpritesWrapping(crop, maskMiragecrops4, cropSarracenia, 5, 5));
+		crop.setHandlerHarvest(
+			new HandlerHarvestOneItem(crop, new ItemStack(craftingLeaf)));
+
+		crop = (CropMirage) cropRoseQuartz;
+		crop.setStatusRegular("Rose Quartz", 7, "Mirrgie Riana");
+		crop.setStatusCrossing(2, 0, 1, 3, 0, "Roze");
+		crop.setMaxSize(4);
+		crop.setHandlerSprites(
+			new HandlerSpritesBasic(crop, maskMiragecrops4));
+		crop.setHandlerHarvest(
+			new HandlerHarvestOneItem(crop, new ItemStack(craftingLeaf)));
+
+	}
+
 	@Override
 	public void handle(FMLInitializationEvent event)
 	{
 
 		snw.registerMessage(MessageHandler.class, MessageDataViewInt.class, 0, Side.CLIENT);
+
+		// /give ForgeDevName IC2:itemCropSeed 1 0 {id:80,growth:31,gain:0,resistance:0,scan:4}
+
+		registerCrops();
+
+		configureCrops();
 
 	}
 
