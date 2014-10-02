1139b94e9ebb4485e9d1f9306ad4bda9b162af56
MirrgieRiana <devnull@localhost>
2014-10-02 19:49:46 +0900

    lledo il RegisterMaterialColor in api.oregen

---
+++ src/main/java/mirrg/miragecrops4/api/oregen/RegisterMaterialColor.java
@@ -0,0 +1,10 @@
+package mirrg.miragecrops4.api.oregen;
+
+import java.util.Hashtable;
+
+public class RegisterMaterialColor
+{
+
+	public static Hashtable<String, Integer> instance = new Hashtable<String, Integer>();
+
+}
