2f78641ec80da0f5bfc3973cad229b25b87305b1
MirrgieRiana <devnull@localhost>
2014-10-10 04:21:27 +0900

    FeebleTorch'ie classa ri jucto il getSubBlocks

--- src/main/java/mirrg/miragecrops4/core/BlockTorchFeeble.java
+++ src/main/java/mirrg/miragecrops4/core/BlockTorchFeeble.java
@@ -1,8 +1,15 @@
 package mirrg.miragecrops4.core;
 
+import java.util.List;
+
 import mirrg.mir40.math.HelpersMath;
 import net.minecraft.block.BlockTorch;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.world.IBlockAccess;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 
 public class BlockTorchFeeble extends BlockTorch
 {
@@ -22,6 +29,15 @@ public class BlockTorchFeeble extends BlockTorch
 	public int damageDropped(int p_149692_1_)
 	{
 		return p_149692_1_;
+	}
+
+	@Override
+	@SideOnly(Side.CLIENT)
+	public void getSubBlocks(Item item, CreativeTabs p_149666_2_, List list)
+	{
+		for (int i = 0; i <= 15; i++) {
+			list.add(new ItemStack(item, 1, i));
+		}
 	}
 
 }
