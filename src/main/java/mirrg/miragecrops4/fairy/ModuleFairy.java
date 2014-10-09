5d5d287662eaa7dd7b0e2ece0a3d1117ef7af7d2
MirrgieRiana <devnull@localhost>
2014-10-10 02:38:52 +0900

    ModuleFairy ri extendo il ModuleMirageCropsBase

--- src/main/java/mirrg/miragecrops4/fairy/ModuleFairy.java
+++ src/main/java/mirrg/miragecrops4/fairy/ModuleFairy.java
@@ -5,25 +5,23 @@ import static mirrg.miragecrops4.api.oregen.ItemsOregen.*;
 import java.util.HashSet;
 
 import mirrg.mir34.modding.IMod;
-import mirrg.mir34.modding.ModuleAbstract;
 import mirrg.mir40.glob.api.HelpersGlob;
 import mirrg.mir40.net.MessageFieldInt;
 import mirrg.miragecrops4.api.oregen.ItemsOregen.EnumGlobsCalciteGroup;
 import mirrg.miragecrops4.api.oregen.ItemsOregen.EnumGlobsMirageMagic;
 import mirrg.miragecrops4.api.oregen.ItemsOregen.EnumGlobsMirageMaterial;
 import mirrg.miragecrops4.core.ModuleCore;
+import mirrg.miragecrops4.core.ModuleMirageCropsBase;
 import mirrg.miragecrops4.fairy.glass.HandlerRenderingFairyGlass;
 import mirrg.miragecrops4.fairy.glass.ItemFairyGlass;
 import mirrg.miragecrops4.fairy.glass.MessageHandlerFairyGlass;
 import net.minecraft.item.Item.ToolMaterial;
-import net.minecraft.item.ItemArmor;
 import net.minecraft.item.ItemTool;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.oredict.OreDictionary;
 import net.minecraftforge.oredict.ShapedOreRecipe;
 import net.minecraftforge.oredict.ShapelessOreRecipe;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
-import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.network.NetworkRegistry;
 import cpw.mods.fml.common.network.simpleimpl.SimpleNetworkWrapper;
@@ -31,16 +29,12 @@ import cpw.mods.fml.common.registry.GameRegistry;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 
-public class ModuleFairy extends ModuleAbstract
+public class ModuleFairy extends ModuleMirageCropsBase
 {
-
-	public static SimpleNetworkWrapper snw;
 
 	public ModuleFairy(IMod mod)
 	{
 		super(mod);
-
-		snw = NetworkRegistry.INSTANCE.newSimpleChannel(getMod().getModId());
 
 	}
 
@@ -50,42 +44,17 @@ public class ModuleFairy extends ModuleAbstract
 		return "fairy";
 	}
 
-	public static ItemArmor craftingFairyGlass;
+	//
 
-	public static ItemTool craftingToolHardHammerSpinachium;
+	public static SimpleNetworkWrapper snw;
 
 	@Override
 	public void handle(FMLPreInitializationEvent event)
 	{
 
-		{
-			String name = "craftingFairyGlass";
-			ItemFairyGlass item = new ItemFairyGlass();
+		snw = NetworkRegistry.INSTANCE.newSimpleChannel(getMod().getModId());
 
-			item.setUnlocalizedName(name);
-			item.setTextureName(getMod().getModId() + ":" + getModuleName() + "/" + name);
-			item.setCreativeTab(ModuleCore.creativeTab);
-			GameRegistry.registerItem(item, name);
-
-			item.armorTexture1 = getMod().getModId() + ":textures/models/armor/" + getModuleName() + "/" + name + "_1.png";
-			item.armorTexture2 = getMod().getModId() + ":textures/models/armor/" + getModuleName() + "/" + name + "_2.png";
-			craftingFairyGlass = item;
-		}
-
-		{
-			String name = "craftingToolHardHammerSpinachium";
-			ItemTool item = new ItemTool(1, ToolMaterial.IRON, new HashSet()) {
-
-			};
-
-			item.setUnlocalizedName(name);
-			item.setTextureName(getMod().getModId() + ":" + getModuleName() + "/" + name);
-			item.setCreativeTab(ModuleCore.creativeTab);
-			GameRegistry.registerItem(item, name);
-
-			OreDictionary.registerOre("craftingToolHardHammer", item);
-			craftingToolHardHammerSpinachium = item;
-		}
+		super.handle(event);
 
 	}
 
@@ -94,6 +63,8 @@ public class ModuleFairy extends ModuleAbstract
 	{
 
 		snw.registerMessage(MessageHandlerFairyGlass.class, MessageFieldInt.class, 0, Side.CLIENT);
+
+		super.handle(event);
 
 	}
 
@@ -104,10 +75,47 @@ public class ModuleFairy extends ModuleAbstract
 
 		MinecraftForge.EVENT_BUS.register(new HandlerRenderingFairyGlass());
 
+		super.handleClient(event);
+
+	}
+
+	//
+
+	public static ItemFairyGlass craftingFairyGlass;
+	public static ItemTool craftingToolHardHammerSpinachium;
+
+	@Override
+	protected void registerItems()
+	{
+		craftingFairyGlass = registerItem(new ItemFairyGlass(), "craftingFairyGlass");
+		craftingToolHardHammerSpinachium = registerItem(
+			new ItemTool(1, ToolMaterial.IRON, new HashSet()) {
+
+			}, "craftingToolHardHammerSpinachium");
 	}
 
 	@Override
-	public void handle(FMLPostInitializationEvent event)
+	protected void configureItems()
+	{
+
+		{
+			String name = "craftingFairyGlass";
+			configureItem(craftingFairyGlass, name);
+			craftingFairyGlass.setCreativeTab(ModuleCore.creativeTab);
+			craftingFairyGlass.armorTexture1 =
+				getMod().getModId() + ":textures/models/armor/" + getModuleName() + "/" + name + "_1.png";
+			craftingFairyGlass.armorTexture2 =
+				getMod().getModId() + ":textures/models/armor/" + getModuleName() + "/" + name + "_2.png";
+		}
+
+		configureItem(craftingToolHardHammerSpinachium, "craftingToolHardHammerSpinachium");
+		craftingToolHardHammerSpinachium.setCreativeTab(ModuleCore.creativeTab);
+		OreDictionary.registerOre("craftingToolHardHammer", craftingToolHardHammerSpinachium);
+
+	}
+
+	@Override
+	protected void registerRecipes()
 	{
 
 		GameRegistry.addRecipe(new ShapedOreRecipe(craftingFairyGlass,
