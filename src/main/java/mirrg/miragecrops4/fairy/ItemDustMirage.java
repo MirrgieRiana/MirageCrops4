984a890759300169544d36737eba44db4e5ac5ca
MirrgieRiana <devnull@localhost>
2014-10-12 20:06:01 +0900

    dustMirage'ia ri trrodlo il Item iv pia ri rightClickoreloetocha

--- src/main/java/mirrg/miragecrops4/fairy/ItemDustMirage.java
+++ src/main/java/mirrg/miragecrops4/fairy/ItemDustMirage.java
@@ -1,8 +1,42 @@
 package mirrg.miragecrops4.fairy;
 
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Items;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
 
 public class ItemDustMirage extends Item
 {
+
+	public ItemStack createChangedItem()
+	{
+		return new ItemStack(Items.potionitem);
+	}
+
+	@Override
+	public ItemStack onItemRightClick(ItemStack p_77659_1_, World p_77659_2_, EntityPlayer p_77659_3_)
+	{
+		--p_77659_1_.stackSize;
+
+		p_77659_2_.playSoundEffect(
+			(double) ((float) p_77659_3_.posX + 0.5F),
+			(double) ((float) p_77659_3_.posY + 0.5F),
+			(double) ((float) p_77659_3_.posZ + 0.5F),
+			"random.fizz",
+			0.5F, 2.6F + (p_77659_2_.rand.nextFloat() - p_77659_2_.rand.nextFloat()) * 0.8F);
+
+		if (p_77659_1_.stackSize <= 0)
+		{
+			return createChangedItem();
+		}
+
+		if (!p_77659_3_.inventory.addItemStackToInventory(createChangedItem()))
+		{
+			p_77659_3_.dropPlayerItemWithRandomChoice(createChangedItem(), false);
+		}
+
+		return p_77659_1_;
+	}
 
 }
