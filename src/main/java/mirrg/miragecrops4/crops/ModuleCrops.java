aa21509289eb0dbd994ec237bf304a993bbb23a6
MirrgieRiana <devnull@localhost>
2014-10-14 02:25:12 +0900

    cropoja ri chso ik (sarracenia, lightning sarracenia, rose quartz) in (sarracenia, spinach), cropie lyme patha ri moizo

--- src/main/java/mirrg/miragecrops4/crops/ModuleCrops.java
+++ src/main/java/mirrg/miragecrops4/crops/ModuleCrops.java
@@ -6,7 +6,6 @@ import mirrg.mir34.modding.IMod;
 import mirrg.mir40.crop.CropMirage;
 import mirrg.mir40.crop.HandlerHarvestOneItem;
 import mirrg.mir40.crop.HandlerSpritesBasic;
-import mirrg.mir40.crop.HandlerSpritesWrapping;
 import mirrg.miragecrops4.core.ModuleCore;
 import mirrg.miragecrops4.core.ModuleMirageCropsBase;
 import mirrg.miragecrops4.crops.fairy.CropMirageFairyHousing;
@@ -55,8 +54,7 @@ public class ModuleCrops extends ModuleMirageCropsBase
 	public static Item craftingLeaf;
 
 	public static CropCard cropSarracenia;
-	public static CropCard cropLightningSarracenia;
-	public static CropCard cropRoseQuartz;
+	public static CropCard cropSpinach;
 
 	@Override
 	protected void registerItems()
@@ -78,40 +76,30 @@ public class ModuleCrops extends ModuleMirageCropsBase
 
 	protected void registerCrops()
 	{
-		cropSarracenia = registerCrop(80, new CropMirageCropsBase(), "Sarracenia");
-		cropLightningSarracenia = registerCrop(81, new CropMirageFairyHousing(), "Lightning Sarracenia");
-		cropRoseQuartz = registerCrop(100, new CropMirageFairyWorkplace(), "Rose Quartz");
+		cropSarracenia = registerCrop(80, new CropMirageFairyHousing(), "Sarracenia");
+		cropSpinach = registerCrop(81, new CropMirageFairyWorkplace(), "Spinach");
 	}
 
 	protected void configureCrops()
 	{
 		// /give ForgeDevName IC2:itemCropSeed 1 0 {id:80,growth:31,gain:0,resistance:0,scan:4}
 
-		String maskMiragecrops4 = "miragecrops4:crop/blockCrop.%name%.%size%";
+		String maskMiragecrops4 = getMod().getModId() + ":" + getModuleName() + "/blockCrop.%name%.%size%";
 
 		CropMirageCropsBase crop;
 
 		crop = (CropMirageCropsBase) cropSarracenia;
 		crop.setStatusRegular(3, "Mirrgie Riana");
-		crop.setStatusCrossing(0, 0, 3, 1, 2, "Sarracenia");
+		crop.setStatusCrossing(0, 0, 3, 1, 2, "Sarracenia", "Green");
 		crop.setMaxSize(5);
 		crop.setHandlerSprites(
 			new HandlerSpritesBasic(crop, maskMiragecrops4));
 		crop.setHandlerHarvest(
 			new HandlerHarvestOneItem(crop, new ItemStack(craftingLeaf)));
 
-		crop = (CropMirageCropsBase) cropLightningSarracenia;
-		crop.setStatusRegular(5, "Mirrgie Riana");
-		crop.setStatusCrossing(1, 0, 3, 2, 2, "Sarracenia", "Lightning", "Purple");
-		crop.setMaxSize(5);
-		crop.setHandlerSprites(
-			new HandlerSpritesWrapping(crop, maskMiragecrops4, cropSarracenia, 5, 5));
-		crop.setHandlerHarvest(
-			new HandlerHarvestOneItem(crop, new ItemStack(craftingLeaf)));
-
-		crop = (CropMirageCropsBase) cropRoseQuartz;
-		crop.setStatusRegular(7, "Mirrgie Riana");
-		crop.setStatusCrossing(2, 0, 1, 3, 0, "Roze");
+		crop = (CropMirageCropsBase) cropSpinach;
+		crop.setStatusRegular(3, "Mirrgie Riana");
+		crop.setStatusCrossing(0, 2, 0, 1, 0, "Spinach", "Green");
 		crop.setMaxSize(4);
 		crop.setHandlerSprites(
 			new HandlerSpritesBasic(crop, maskMiragecrops4));
