7ed280273289086168f29d82cd9b3c8bca3e8721
MirrgieRiana <devnull@localhost>
2014-10-14 19:20:01 +0900

    Version4.0.2(October Update)
    ・マテリアルにブロック、インゴット、粉末、宝石の形状を追加
    ・妖精の眼鏡追加
    ・スピナチウムハンマー追加
    ・Cropを2種類追加
    　・サラセニア　付近の小松菜の成長速度増加
    　・小松菜　付近にサラセニアがあると成長速度増加
    ・ミラージュの粉追加
    ・Modダンパーの同封
    ・API、ライブラリの更新
    ・弱いトーチは光量に幅を持つように
    ・他多数

--- src/main/java/mirrg/miragecrops4/core/ModMirageCrops4.java
+++ src/main/java/mirrg/miragecrops4/core/ModMirageCrops4.java
@@ -20,7 +20,7 @@ public class ModMirageCrops4 extends ModAbstract
 {
 
 	public static final String MODID = "miragecrops4";
-	public static final String VERSION = "4.0.1";
+	public static final String VERSION = "4.0.2";
 	public static final String NAME = "MirageCrops4";
 	public static final String DEPENDENCIES =
 		"after:IC2;" +
