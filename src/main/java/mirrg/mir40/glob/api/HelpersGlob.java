cd4ea3778cfc6220b429fd56521870a4871f9ae6
MirrgieRiana <devnull@localhost>
2014-10-15 23:32:02 +0900

    lledo 'copy'ienmethodau in HelpersGlob

--- src/main/java/mirrg/mir40/glob/api/HelpersGlob.java
+++ src/main/java/mirrg/mir40/glob/api/HelpersGlob.java
@@ -1,6 +1,7 @@
 package mirrg.mir40.glob.api;
 
 import mirrg.mir40.math.HelpersString;
+import net.minecraft.item.ItemStack;
 
 public class HelpersGlob
 {
@@ -8,6 +9,18 @@ public class HelpersGlob
 	public static String getDictionaryName(ISlot slot, IGlob glob)
 	{
 		return slot.getName() + HelpersString.toUpperCaseHead(glob.getName());
+	}
+
+	public static ItemStack copy(ISlot slot, IGlob glob, int stackSize)
+	{
+		ItemStack itemStack = glob.copy(slot);
+		itemStack.stackSize = stackSize;
+		return itemStack;
+	}
+
+	public static ItemStack copy(ISlot slot, IGlob glob)
+	{
+		return copy(slot, glob, 1);
 	}
 
 }
