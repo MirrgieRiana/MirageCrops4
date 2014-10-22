171535c55ca66f3911864f6de4c7b3f300c7926c
MirrgieRiana <devnull@localhost>
2014-10-23 02:50:54 +0900

    lledo il mirrgw.plexe

---
+++ mirrg.config.pl
@@ -0,0 +1,5 @@
+[
+	['build.gradle', qr/(version = ")([0-9.]+)(")/],
+	['src/main/resources/mcmod.info', qr/("version": ")([0-9.]+)(")/],
+	['src/main/java/mirrg/miragecrops4/core/ModMirageCrops4.java', qr/(VERSION = ")([0-9.]+)(")/],
+]
