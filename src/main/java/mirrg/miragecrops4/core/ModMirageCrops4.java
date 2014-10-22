f5489e2b9dd2b7c15f0afb5c0bb7e1ac405d204d
MirrgieRiana <devnull@localhost>
2014-10-23 03:02:52 +0900

    Version4.0.4(Material Update)
    ・DumperにIC2Cropの交配レートを追加
    ・素材アイテムに関する内部処理を改善
    ・弱い松明を色々更新

--- src/main/java/mirrg/miragecrops4/core/ModMirageCrops4.java
+++ src/main/java/mirrg/miragecrops4/core/ModMirageCrops4.java
@@ -20,7 +20,7 @@ public class ModMirageCrops4 extends ModAbstract
 {
 
 	public static final String MODID = "miragecrops4";
-	public static final String VERSION = "4.0.3";
+	public static final String VERSION = "4.0.4";
 	public static final String NAME = "MirageCrops4";
 	public static final String DEPENDENCIES =
 		"after:IC2;" +
