782cdeb159d8bf1d6eafaad9dfd96959d48c4ca0
MirrgieRiana <devnull@localhost>
2014-10-28 05:33:44 +0900

    hysho il HelpersOregen

--- src/main/java/mirrg/miragecrops4/lib/oregen/HelpersOregen.java
+++ src/main/java/mirrg/miragecrops4/lib/oregen/HelpersOregen.java
@@ -2,27 +2,18 @@ package mirrg.miragecrops4.lib.oregen;
 
 import mirrg.mir40.math.HelpersString;
 import mirrg.mir40.reflect.HelpersReflect;
-import mirrg.mir41.glob.IGlob;
 import mirrg.mir41.glob.IGlobGroup;
 import mirrg.mir41.glob.ISlot;
 import mirrg.miragecrops4.lib.multi.BlockMultiOregen;
+import mirrg.miragecrops4.lib.multi.ItemMultiIconOregen;
 import mirrg.miragecrops4.lib.multi.MetablockOregen;
+import mirrg.miragecrops4.lib.multi.MetaitemIconOregen;
+import mirrg.miragecrops4.lib.oregen.GlobsOregen.EnumGlob;
+import mirrg.miragecrops4.lib.oregen.GlobsOregen.EnumGlobGroup;
 import mirrg.miragecrops4.lib.oregen.GlobsOregen.EnumSlot;
 
 public class HelpersOregen
 {
-
-	public static void overrideMetablock(ISlot slot, IGlobGroup<?> globGroup, IGlob glob, MetablockOregen metablock)
-	{
-		Object blockMulti = HelpersReflect.getStaticField(
-			ItemsOregen.class, HelpersOregen.getBlockUnlocalizedName(slot, globGroup));
-		if (blockMulti == null || blockMulti instanceof Exception) {
-			throw new RuntimeException((Exception) blockMulti);
-		}
-
-		((BlockMultiOregen) blockMulti).multi.clearBindind(globGroup.getGlobs().indexOf(glob));
-		((BlockMultiOregen) blockMulti).multi.bind(globGroup.getGlobs().indexOf(glob), metablock);
-	}
 
 	public static String getBlockUnlocalizedName(ISlot slot, IGlobGroup<?> globGroup)
 	{
@@ -36,6 +27,56 @@ public class HelpersOregen
 	public static String getItemUnlocalizedName(ISlot slot)
 	{
 		return "item" + HelpersString.toUpperCaseHead(slot.getName());
+	}
+
+	public static BlockMultiOregen getBlock(ISlot slot, IGlobGroup<?> globGroup)
+	{
+		Object blockMulti = HelpersReflect.getStaticField(ItemsOregen.class, getBlockUnlocalizedName(slot, globGroup));
+		if (blockMulti == null || blockMulti instanceof Exception) {
+			throw new RuntimeException((Exception) blockMulti);
+		}
+
+		return (BlockMultiOregen) blockMulti;
+	}
+
+	public static ItemMultiIconOregen getItem(ISlot slot)
+	{
+		Object itemMulti = HelpersReflect.getStaticField(ItemsOregen.class, getItemUnlocalizedName(slot));
+		if (itemMulti == null || itemMulti instanceof Exception) {
+			throw new RuntimeException((Exception) itemMulti);
+		}
+
+		return (ItemMultiIconOregen) itemMulti;
+	}
+
+	public static int getMetablockId(EnumGlobGroup enumGlobGroup, EnumGlob enumGlob)
+	{
+		return enumGlobGroup.globGroup.getGlobs().indexOf(enumGlob.glob);
+	}
+
+	public static int getMetaitemId(EnumGlobGroup enumGlobGroup, EnumGlob enumGlob)
+	{
+		return enumGlobGroup.ordinal() * 16 + enumGlobGroup.globGroup.getGlobs().indexOf(enumGlob.glob);
+	}
+
+	public static void overrideMetablock(EnumSlot enumSlot, EnumGlobGroup enumGlobGroup, EnumGlob enumGlob,
+		MetablockOregen metablock)
+	{
+		BlockMultiOregen blockMulti = getBlock(enumSlot.slot, enumGlobGroup.globGroup);
+		int metaId = getMetablockId(enumGlobGroup, enumGlob);
+
+		blockMulti.multi.clearBindind(metaId);
+		blockMulti.multi.bind(enumGlobGroup.globGroup.getGlobs().indexOf(enumGlob), metablock);
+	}
+
+	public static void overrideMetaitem(EnumSlot enumSlot, EnumGlobGroup enumGlobGroup, EnumGlob enumGlob,
+		MetaitemIconOregen metaitem)
+	{
+		ItemMultiIconOregen itemMulti = getItem(enumSlot.slot);
+		int metaId = getMetaitemId(enumGlobGroup, enumGlob);
+
+		itemMulti.multi.clearBindind(metaId);
+		itemMulti.multi.bind(metaId, metaitem);
 	}
 
 }
