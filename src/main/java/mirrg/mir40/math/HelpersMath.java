aaa0a94ba7e3fb95080824d86afc3d8f96e8b3c6
MirrgieRiana <devnull@localhost>
2014-11-04 11:01:38 +0900

    kommento il HelpersMath

--- src/main/java/mirrg/mir40/math/HelpersMath.java
+++ src/main/java/mirrg/mir40/math/HelpersMath.java
@@ -7,11 +7,21 @@ public class HelpersMath
 
 	public static final Random random = new Random();
 
+	/**
+	 * {@link #floorRandom(double, Random)}<br>
+	 * ランダムは{@link #random}を参照する。
+	 */
 	public static int floorRandom(double value)
 	{
 		return floorRandom(value, random);
 	}
 
+	/**
+	 * 複数回呼び出すと平均値がvalueに収束するように、floor(value)以上ceil(value)以下の値を返す。
+	 * 
+	 * @param value
+	 *            0以上の数値
+	 */
 	public static int floorRandom(double value, Random random)
 	{
 		int integer = (int) Math.floor(value);
